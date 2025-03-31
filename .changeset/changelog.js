55cd5c8c6d302180dd55a14ae0f783f315b3ea35
Christian Ivicevic <mail@christian-ivicevic.com>
2025-03-31 16:27:35 +1000

    build: add custom changelog generator

---
+++ .changeset/changelog.js
@@ -0,0 +1,105 @@
+// Copied and adapted from https://github.com/clerk
+
+const { getInfo, getInfoFromPullRequest } = require('@changesets/get-github-info')
+
+const REPOSITORY = 'ChristianIvicevic/intl-watcher'
+
+/**
+ * @type {import('@changesets/types').GetDependencyReleaseLine}
+ */
+const getDependencyReleaseLine = async (changesets, dependenciesUpdated) => {
+	if (dependenciesUpdated.length === 0) {
+		return ''
+	}
+
+	const changesetLink = `- Updated dependencies [${(
+		await Promise.all(
+			changesets.map(async (changeset) => {
+				if (!changeset.commit) {
+					return
+				}
+				const { links } = await getInfo({ repo: REPOSITORY, commit: changeset.commit })
+				return links.commit
+			}),
+		)
+	)
+		.filter((_) => _)
+		.join(', ')}]:`
+
+	const updatedDependenciesList = dependenciesUpdated.map(
+		(dependency) => `  - ${dependency.name}@${dependency.newVersion}`,
+	)
+
+	return [changesetLink, ...updatedDependenciesList].join('\n')
+}
+
+const PR_REGEX = /^\s*(?:pr|pull|pull\s+request):\s*#?(\d+)/im
+const COMMIT_REGEX = /^\s*commit:\s*([^\s]+)/im
+const AUTHOR_REGEX = /^\s*(?:author|user):\s*@?([^\s]+)/gim
+
+/**
+ * @type {import('@changesets/types').GetReleaseLine}
+ */
+const getReleaseLine = async (changeset) => {
+	let prFromSummary
+	let commitFromSummary
+	const usersFromSummary = []
+
+	const replacedChangelog = changeset.summary
+		.replace(PR_REGEX, (_, pr) => {
+			const num = Number(pr)
+			if (!Number.isNaN(num)) {
+				prFromSummary = num
+			}
+			return ''
+		})
+		.replace(COMMIT_REGEX, (_, commit) => {
+			commitFromSummary = commit
+			return ''
+		})
+		.replace(AUTHOR_REGEX, (_, user) => {
+			usersFromSummary.push(user)
+			return ''
+		})
+		.trim()
+
+	const [firstLine, ...futureLines] = replacedChangelog.split('\n').map((l) => l.trimEnd())
+
+	const links = await (async () => {
+		if (prFromSummary !== undefined) {
+			let { links } = await getInfoFromPullRequest({ repo: REPOSITORY, pull: prFromSummary })
+			if (commitFromSummary) {
+				links = {
+					...links,
+					commit: `[\`${commitFromSummary}\`](https://github.com/${REPOSITORY}/commit/${commitFromSummary})`,
+				}
+			}
+			return links
+		}
+		const commitToFetchFrom = commitFromSummary ?? changeset.commit
+		if (commitToFetchFrom) {
+			const { links } = await getInfo({ repo: REPOSITORY, commit: commitToFetchFrom })
+			return links
+		}
+		return { commit: null, pull: null, user: null }
+	})()
+
+	const users =
+		usersFromSummary.length > 0
+			? usersFromSummary
+					.map((userFromSummary) => `[@${userFromSummary}](https://github.com/${userFromSummary})`)
+					.join(', ')
+			: links.user
+
+	const prefix = [links.pull === null ? '' : ` (${links.pull})`, users === null ? '' : ` by ${users}`].join(
+		'',
+	)
+
+	return `\n\n- ${firstLine}${prefix ? `${prefix} \n` : ''}\n${futureLines.map((l) => `  ${l}`).join('\n')}`
+}
+
+/**
+ * @type {import('@changesets/types').ChangelogFunctions}
+ */
+const changelogFunctions = { getReleaseLine, getDependencyReleaseLine }
+module.exports = changelogFunctions
